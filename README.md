0fd1db06646a0b16807677d14904ef2610204ff8
@diogoCesar <39969711+diosdiogo@users.noreply.github.com>
2021-11-02 06:59:33 -0300

    README

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1 align="center">API DE PAGAMENTO ZM PAY</h1>
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("API ZM Pay")
+    //res.send("API ZM Pay")
+    res.sendFile(__dirname + "/html/")
 })
 
 app.listen("3000", function(){
